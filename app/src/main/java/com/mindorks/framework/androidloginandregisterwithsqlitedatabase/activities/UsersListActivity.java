4365a5fed4a007913d0105e5d509b13a70a84a3f
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-12-02 09:49:08 +0530

    create ui of user list

---
+++ app/src/main/java/com/mindorks/framework/androidloginandregisterwithsqlitedatabase/activities/UsersListActivity.java
@@ -0,0 +1,16 @@
+package com.mindorks.framework.androidloginandregisterwithsqlitedatabase.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.mindorks.framework.androidloginandregisterwithsqlitedatabase.R;
+
+public class UsersListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_users_list);
+    }
+}
